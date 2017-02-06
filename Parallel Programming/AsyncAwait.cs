890655a2105846b9fc9c934e90734405abc7a532
shreesharao <sshreesha4@gmail.com>
2017-02-06 19:06:28 +0530

    parallel programming project added

---
+++ Parallel Programming/AsyncAwait.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_Programming
+{
+    class AsyncAwait
+    {
+        public async void SyncMethod()
+        {
+            int result = await AsyncMethod();
+            Console.WriteLine("result of async method:" + result);
+        }
+
+        private async Task<int> AsyncMethod()
+        {
+            try
+            {
+                await Task.Delay(5000);
+                Console.WriteLine("i am from async method");
+                return 1;
+            }
+            finally
+            {
+                Console.WriteLine("Finally is reached");
+            }
+           
+        }
+    }
+}
---
+++ Parallel Programming/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parallel_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string async; //async is an identifier here
+
+            //call sync method
+            new AsyncAwait().SyncMethod();
+            Console.WriteLine("This must be the first line");
+            Console.Read();
+
+            
+        }
+
+       
+    }
+}
