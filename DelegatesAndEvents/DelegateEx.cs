c34c8fbf9252962c0769417a393d461372ee9e30
shreesharao <sshreesha4@gmail.com>
2017-02-21 15:30:03 +0530

    Delegates and events project added

---
+++ DelegatesAndEvents/DelegateEx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    class DelegateEx
+    {
+        private delegate void PrintDelegate();
+
+        public static void Call()
+        {
+            PrintDelegate printDelegate = new PrintDelegate(Print);
+            printDelegate.Invoke();
+        }
+        private static void Print()
+        {
+            Console.WriteLine("Message from Print method");
+        }
+
+    }
+}
