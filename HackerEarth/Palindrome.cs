db52fb69a3396f5b7500309aa2646d1157a31d2d
shreesharao <sshreesha4@gmail.com>
2017-02-28 15:02:35 +0530

    HackerEarth project added

---
+++ HackerEarth/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerEarth
+{
+    class Palindrome
+    {
+        public void CheckPalindrome()
+        {
+            string str = Console.ReadLine().Trim().ToLower();
+
+            //foreach (char c in str)//this is possible because string has a public definition for GetEnumrator
+            //{
+
+            //}
+            char[] arr = str.ToCharArray();
+
+            Array.Reverse(arr);
+            string rev = new string(arr);
+
+            if (str == rev)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ HackerEarth/StringUpperToLower.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerEarth
+{
+    class StringUpperToLower
+    {
+        public void Function()
+        {
+            string str = Console.ReadLine().Trim();
+            char[] charArray = str.ToCharArray();
+            string finalStr = string.Empty;
+            foreach (var chr in charArray)
+            {
+                if (chr >= 65 && chr <= 90)//means character is capital
+                {
+                    finalStr += chr.ToString().ToLower();
+                }
+                else if (chr >= 97 && chr <= 122)
+                {
+                    finalStr += chr.ToString().ToUpper();
+                }
+                else
+                {
+                    finalStr += chr.ToString();
+                }
+            }
+            Console.WriteLine(finalStr);
+        }
+
+    }
+}
