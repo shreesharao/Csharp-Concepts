9d2826845164db33b981548e3464dc37da54b657
shreesharao <sshreesha4@gmail.com>
2017-03-30 15:07:27 +0530

    Generic classes updated

---
+++ Collections/System.Collections.Generic/QueueEx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.System.Collections.Generic
+{
+    class QueueEx
+    {
+        Queue<int> objQueue = new Queue<int>();
+
+        public void Entry()
+        {
+            objQueue.Enqueue(1);
+
+            foreach (var item in objQueue)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Collections/System.Collections.Generic/StackEx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.System.Collections.Generic
+{
+    class StackEx
+    {
+        Stack<string> objStack = new Stack<string>(); //string collection..it is type safe
+
+        public void Entry()
+        {
+            objStack.Push("test");
+
+            foreach (var item in objStack)
+            {
+                Console.WriteLine(item);               
+            }
+        }
+    }
+}
