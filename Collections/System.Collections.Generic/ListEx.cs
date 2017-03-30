5af7fd204e16487f11e9ef722fd119523ae15127
shreesharao <sshreesha4@gmail.com>
2017-03-30 15:08:13 +0530

    Generic classes updated

---
+++ Collections/System.Collections.Generic/ListEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.System.Collections.Generic
+{
+    class ListEx
+    {
+        List<char> objList = new List<char>();
+
+        public void Entry()
+        {
+            objList.Add('s');
+            objList.Add('s');
+            objList.Add('r');
+            foreach (var item in objList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
