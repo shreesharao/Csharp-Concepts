87254b327dfd9923e2228abdfaa2de16a29e32ec
shreesharao <sshreesha4@gmail.com>
2017-06-09 12:01:22 +0530

    windows service project added

---
+++ WindowsServiceEx/InstallerEx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+
+namespace WindowsServiceEx
+{
+    [RunInstaller(true)]
+    public partial class InstallerEx : System.Configuration.Install.Installer
+    {
+        public InstallerEx()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WindowsServiceEx/Service1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace WindowsServiceEx
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.Diagnostics.Debugger.Launch();
+            EventLog.WriteEntry("started");
+        }
+
+        protected override void OnStop()
+        {
+            EventLog.WriteEntry("stopping");
+        }
+    }
+}
