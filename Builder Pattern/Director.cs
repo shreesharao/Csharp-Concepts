44a9b2ed1ccfd2e7517b9e4b2c31ef3a232e338d
shreesharao <sshreesha4@gmail.com>
2017-07-10 09:26:27 +0530

    Builder pattern added

---
+++ Builder Pattern/Director.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Builder_Pattern
+{
+    class Director
+    {
+        public void Construct(IBuilder IBuilder)
+        {
+            IBuilder.BuildPart1();
+            IBuilder.BuildPart2();
+            IBuilder.BuildPart3();
+        }
+    }
+}
---
+++ Builder Pattern/IBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Builder_Pattern
+{
+    interface IBuilder
+    {
+        void BuildPart1();
+        void BuildPart2();
+        void BuildPart3();
+        Product GetProduct();
+    }
+}
