d4ed754b8d379a8472e2014ad46c23d2788b76f6
shreesharao <sshreesha4@gmail.com>
2017-08-04 17:08:27 +0530

    Nlog related project added

---
+++ NLogHelper/FileLogging.cs
@@ -0,0 +1,25 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NLogHelper
+{
+    class FileLogging
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        //private static Logger logger = LogManager.GetLogger("FileLoging");
+
+        public void Entry()
+        {
+            LogToFile();
+        }
+
+        public void LogToFile()
+        {
+            logger.Debug("Debug Message from FileLogging");
+            //logger.Log(LogLevel.Debug, "Message from FileLogging"); //we can specify log level
+        }
+    }
+}
---
+++ NLogHelper/LoggerSpecificRouting.cs
@@ -0,0 +1,23 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NLogHelper
+{
+    class LoggerSpecificRouting
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public void Entry()
+        {
+            LogToFile();
+        }
+
+        public void LogToFile()
+        {
+            logger.Trace("Trace message from LoggerSpceficRouting");
+        }
+    }
+}
