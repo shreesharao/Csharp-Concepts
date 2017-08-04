afdad1c0223a69267059978d0228f7ee8ba95807
shreesharao <sshreesha4@gmail.com>
2017-08-04 17:05:25 +0530

    httpmodule readme updated

--- Builder Pattern/Program.cs
+++ Builder Pattern/Program.cs
@@ -9,6 +9,33 @@ namespace Builder_Pattern
     {
         static void Main(string[] args)
         {
+           new Client().Entry();
+            Console.Read();
+        }
+    }
+
+    class Client
+    {
+        Manufacturer objManufacturer =null;
+
+        public void Entry()
+        {
+            objManufacturer = new Manufacturer();
+            objManufacturer.Manufacture(new CarBuilder());
+            var car = objManufacturer.GetVehicle();
+            car.GetInfo();
+            AddCharacter('*', 20);
+
+            objManufacturer.Manufacture(new SupercarBuilder());
+            Vehicle objVehicle = objManufacturer.GetVehicle();
+            var superCar = objManufacturer.GetVehicle();
+            superCar.GetInfo();
+            AddCharacter('*', 20);
+        }
+
+        public void AddCharacter(char ch,int count)
+        {
+            Console.WriteLine(new string(ch, count));
         }
     }
 }
--- ExHTTPModule/readme.txt
+++ ExHTTPModule/readme.txt
@@ -1,5 +1,6 @@
 ﻿How to run this?
 
+
 Way 1:
 Copy this dll to webapplication bin direcotry
 Add the below entry in the application web.config
@@ -10,7 +11,7 @@ Add the below entry in the application web.config
 
 
 Way 2:
-Add this assembly to GAC
+Add this assembly to GAC.To add to GAC it should be strong named.
 In IIS Navigate to Your website >Modules 
 Right hand side click on 'Add Managed Module'
 Specify the type as "ExHTTPModule.Module, ExHTTPModule, Version=1.0.0.0, Culture=neutral, PublicKeyToken=9d7490a46e3a39df"
