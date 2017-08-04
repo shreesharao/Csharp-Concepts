8017adba6733a28acd1f510c872c0c0223c36d4f
shreesharao <sshreesha4@gmail.com>
2017-08-04 17:06:56 +0530

    builder pattern added

---
+++ Builder Pattern/CarBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Builder_Pattern
+{
+    class CarBuilder:IBuilder
+    {
+        private Vehicle _car = new Vehicle();
+
+        public void SetType()
+        {
+            _car.Type = "Car";
+        }
+
+        public void SetColor()
+        {
+            _car.Color = "Red";
+        }
+
+        public void SetEngine()
+        {
+            _car.Engine = "4 stroke";
+        }
+
+        public void SetModel()
+        {
+            _car.Model = "2017";
+        }
+
+        public void SetTopSpeed()
+        {
+            _car.TopSpeed = 200;
+        }
+
+        public void SetAccessories()
+        {
+            _car.accessories.Add("sterio");
+        }
+
+        public Vehicle GetVehicle()
+        {
+            return _car;
+        }
+    }
+}
