7fd8506feb0402311f3a8c92ba3a9909afff044e
shreesharao <sshreesha4@gmail.com>
2017-08-10 21:20:32 +0530

    system.io.pipes client and server created

---
+++ PipeClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+namespace PipeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting client");
+            StartClient();
+        }
+
+        private static void StartClient()
+        {
+            var client = new NamedPipeClientStream("PipesOfPiece");
+            Console.WriteLine("trying to connect to server");
+            client.Connect();
+            Console.WriteLine("Connected to server");
+            StreamReader reader = new StreamReader(client);
+            StreamWriter writer = new StreamWriter(client);
+
+            string input = "test line from client";
+            writer.WriteLine(input);
+            writer.Flush();
+            //while (true)
+            //{
+            //    string line = reader.ReadLine();
+            //    Console.WriteLine("Read:"+line);
+            //}
+            //Task.Run(() =>
+            //{
+            //    while (true)
+            //    {
+            //        string input = "test line from client";
+            //        writer.WriteLine(input);
+            //        writer.Flush();
+            //    }
+            //});
+
+
+            //Task.Run(() =>
+            //{
+            //    while (true)
+            //    {
+            //        string line = reader.ReadLine();
+            //        Console.WriteLine(line);
+            //    }
+            //});
+
+        }
+
+
+    }
+}
---
+++ PipeServer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+namespace PipeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting server");
+            StartServer();
+            Console.Read();
+        }
+
+        private static void StartServer()
+        {
+            var server = new NamedPipeServerStream("PipesOfPiece");
+            Console.WriteLine("started waiting for client to connect ");
+            //server.WaitForConnection();
+           var result= server.BeginWaitForConnection(BeginWaitForConnectionCallBack, server);
+          
+            //Task.Run(() =>
+            //{
+            //    while (true)
+            //    {
+            //        string input = "test line from server";
+            //        writer.WriteLine(input);
+            //        writer.Flush();
+            //    }
+            //});
+
+
+            //Task.Run(() =>
+            //{
+            //    while (true)
+            //    {
+            //        string line = reader.ReadLine();
+            //        Console.WriteLine(line);
+            //    }
+            //});
+            
+        }
+
+        private static void BeginWaitForConnectionCallBack(IAsyncResult ar)
+        {
+            Console.WriteLine("client connected");
+            StreamReader reader = new StreamReader((NamedPipeServerStream)ar.AsyncState);
+            StreamWriter writer = new StreamWriter((NamedPipeServerStream)ar.AsyncState);
+            //while (true)
+            //{
+            //    string input = "test line from server";
+            //    writer.WriteLine(input);
+            //    writer.Flush();
+            //}
+
+            var line = reader.ReadLine();
+            Console.WriteLine("Read:"+line);
+
+         
+        }
+    }
+}
