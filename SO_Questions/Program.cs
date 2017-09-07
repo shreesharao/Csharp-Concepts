d0d3c5c943b809570866f09085888fc8deaf5f02
shreesharao <sshreesha4@gmail.com>
2017-09-07 16:33:44 +0530

    C# indexer covered

--- SO_Questions/Program.cs
+++ SO_Questions/Program.cs
@@ -25,7 +25,8 @@ namespace DotNet_store
             //new Topic_14().Entry();
             //new Topic_15().Entry();
             //new Topic_16().Entry();
-            new Topic_17().Entry();
+            //new Topic_17().Entry();
+            new Topic_18().Entry();
             Console.Read();
         }
     }
---
+++ SO_Questions/Topic 18.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_store
+{
+    class Topic_18
+    {
+        public void Entry()
+        {
+            Topic_18_TestClass<string> obj = new Topic_18_TestClass<string>();
+            obj[0] = "test";
+
+            Console.WriteLine(obj[0]);
+            Console.WriteLine(obj["test"]);
+        }
+    }
+
+    public interface ISomeInterface<T>
+    {
+        //...
+
+        // Indexer declaration:
+        T this[int index]
+        {
+            get;
+            set;
+        }
+    }
+
+    class Topic_18_TestClass<T>:ISomeInterface<T>
+    {
+        private T[] _genArray = new T[100];
+        public T this[int i]
+        {
+            get
+            {
+                return _genArray[i];
+            }
+            set
+            {
+                _genArray[i] = value;
+            }
+        }
+
+        //indexer overload
+        public T this[string key]
+        {
+            get
+            {
+                return GetValue(key);
+            }
+            
+        }
+
+        public T GetValue(string key)
+        {
+            T ret=default(T);
+            ret= _genArray.Where(v => v.ToString() == key).FirstOrDefault();
+            return ret;
+        }
+
+       
+    }
+}
